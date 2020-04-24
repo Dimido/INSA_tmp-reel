b8469781222609166d08d9bb57192bac620a3d9a
geekcv <36602541+paulEtse@users.noreply.github.com>
2020-04-24 19:45:15 +0200

    Update tasks.h

--- software/raspberry/superviseur-robot/tasks.h
+++ software/raspberry/superviseur-robot/tasks.h
@@ -86,14 +86,10 @@ private:
     RT_MUTEX mutex_robot;
     RT_MUTEX mutex_robotStarted;
     RT_MUTEX mutex_move;
-<<<<<<< HEAD
     RT_MUTEX mutex_batterie;
     //Custom
     RT_MUTEX mutex_camera;
-=======
     RT_MUTEX mutex_compteur;
->>>>>>> c014f37a2b345c194e7ac0fd84538f3cc7e3a313
-
     /**********************************************************************/
     /* Semaphores                                                         */
     /**********************************************************************/
